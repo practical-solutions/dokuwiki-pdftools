ed981b68ffd4e52ca75a3f7ae342a8215d6c8bbc
Gero Gothe <practical@medizin-lernen.de>
2020-06-02 11:25:55 +0200

    Added compatibility with Approve-Plus-Plugin: @APPROVER@-Replacement. Updated Templates: Added @APPROVER@-Replacement to templates: checkliste, info, standard

--- action.php
+++ action.php
@@ -13,27 +13,27 @@ if(!defined('DOKU_INC')) die();
 
 class action_plugin_pdftools extends DokuWiki_Action_Plugin {
 
-    /**
-     * Register callbacks
-     */
     public function register(Doku_Event_Handler $controller) {
-		
-		$controller->register_hook('PLUGIN_DW2PDF_REPLACE', 'BEFORE', $this, 'replacement_before');
-				
+
+        $controller->register_hook('PLUGIN_DW2PDF_REPLACE', 'BEFORE', $this, 'replacement_before', null, 100);
+
     }
-    
-    
-    
+
+
     function replacement_before(Doku_Event $event, $param) {
 		global $conf;
 		global $INFO;
-			
-		$event->data['replace']['@COMPANY@'] = $conf['plugin']['pdftools']['company'];	
-		$event->data['replace']['@AUTHOR@'] = $INFO['meta']['contributor'][$INFO['user']];
+
+        $event->data['replace']['@COMPANY@'] = $conf['plugin']['pdftools']['company'];	
+        $event->data['replace']['@AUTHOR@'] = $INFO['meta']['contributor'][$INFO['user']];
+
+        # Compatibility with approve-plus-plugin: if not installed, the @APPROVER@-Tag is "removed"
+        if (!isset($event->data['replace']['@APPROVER@'])) $event->data['replace']['@APPROVER@'] = '';
+
+        # Remove Approve-Tag also, if author = approver
+        if (strpos($event->data['replace']['@APPROVER@'],$event->data['replace']['@AUTHOR@']) > 0) $event->data['replace']['@APPROVER@'] = '';
+
 	}
-    
-	
-	
    
 }
 
